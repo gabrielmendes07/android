32554f1d812a156839137a3afaf3ea17b6b2bc7d
gabrielmendes07 <gabrielmendesdac@gmail.com>
2019-03-14 03:37:20 -0300

    android

---
+++ MyApplication3/app/src/main/java/com/example/aluno/myapplication/MainActivity.kt
@@ -0,0 +1,17 @@
+package Loginapp.example.fslbpms.com.br.exampleloginapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import org.jetbrains.anko.toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    this.usarname - findViewById<TextView>(R.id.etLogin)
+
+    }
+}
